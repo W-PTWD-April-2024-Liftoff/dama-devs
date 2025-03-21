cebb1fadbe247db50ce2797aaec07a00423d831a
Dhana bayya <krishnabayya@MacBookAir.lan>
2025-03-21 12:30:35 -0500

    partly implemented oauth

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,23 +1,43 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./components/Home";
 import Dashboard from "./components/Dashboard";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import ExampleOne from './pages/ExampleOne';
+import ExampleTwo from './pages/ExampleTwo';
+import Props from './reusable-code/props';
+import WeatherWidget from './reusable-code/WeatherWidget';
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    // <div>
-    //   <Home />
-    // </div>
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+    </Router>
+  );
+
+    <Router>
+      <WeatherWidget />
+      {/* test for using props
+      <Props />      */}
+      <div>
+        <Routes>
+
+          {/* "/"  will be the default for startup, so I added this route to take me to first page. */}
+          <Route path="/" element={<Navigate to="/chocolate-ice-cream" />} /> {/* Redirect */}
+
+          {/* Define the route for ExampleOne - intentionally naming it somthing completely different so you can see the React routes can have different names.*/}
+          <Route path="/chocolate-ice-cream" element={<ExampleOne />} />
+
+          {/* Define the route for ExampleTwo - intentionally naming it somthing completely different so you can see the React routes can have different names.*/}
+          <Route path="/jolly-ranchers" element={<ExampleTwo />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
