55111e30e7ccdaea5634f83218949b16633a70e5
Allyson Ditchey <166686180+allysonhd@users.noreply.github.com>
2025-03-23 14:22:18 -0500

    Update App.jsx
    As as team we edited this file.

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -8,39 +8,22 @@ import ExampleOne from './pages/ExampleOne';
 import ExampleTwo from './pages/ExampleTwo';
 import Props from './reusable-code/props';
 import WeatherWidget from './reusable-code/WeatherWidget';
-
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
-import ExampleOne from './pages/ExampleOne';
-import ExampleTwo from './pages/ExampleTwo';
-import Props from './reusable-code/props';
-import WeatherWidget from './reusable-code/WeatherWidget';
 
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-  );
-
+   
     <Router>
       <WeatherWidget />
-      {/* test for using props
-      <Props />      */}
 
-    <Router>
-      <WeatherWidget />
-      {/* test for using props
-      <Props />      */}
       <div>
         <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
 
           {/* "/"  will be the default for startup, so I added this route to take me to first page. */}
-          <Route path="/" element={<Navigate to="/chocolate-ice-cream" />} /> {/* Redirect */}
+{/*           <Route path="/" element={<Navigate to="/chocolate-ice-cream" />} /> {/* Redirect */} */}
 
           {/* Define the route for ExampleOne - intentionally naming it somthing completely different so you can see the React routes can have different names.*/}
           <Route path="/chocolate-ice-cream" element={<ExampleOne />} />
